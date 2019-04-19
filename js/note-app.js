1db6c78d957878c4243404883150ad77123dd0fb
Daniel Kulangiev <danny.kulangiev@gmail.com>
2019-04-19 14:08:34 -0400

    Refractor notes and todo apps to utilize DOM manipulation.

--- js/note-app.js
+++ js/note-app.js
@@ -1,7 +1,45 @@
 // DOM - Document Object Model
 
-const p = document.querySelectorAll('p')
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise, Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 
-p.forEach(para => (
-    para.textContent = 'spam'
-))
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = (notes, filters) => {
+    const filteredNotes = notes.filter(note => {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(note => {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', e => {
+
+})
+
+document.querySelector('#remove-all').addEventListener('click', e => {
+
+})
+
+document.querySelector('#search-text').addEventListener('input', e => {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
